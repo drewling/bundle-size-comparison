90e95c0c95f8a2d8adff5c06dc2722dddf3378e4
vzxw <vlad@drewl.com>
2023-06-15 00:14:05 +0500

    init

---
+++ faust-example-app/next.config.js
@@ -0,0 +1,24 @@
+const { withFaust, getWpHostname } = require("@faustwp/core");
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: true,
+});
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+module.exports = withBundleAnalyzer(
+  withFaust({
+    reactStrictMode: true,
+    sassOptions: {
+      includePaths: ["node_modules"],
+    },
+    images: {
+      domains: [getWpHostname()],
+    },
+    i18n: {
+      locales: ["en"],
+      defaultLocale: "en",
+    },
+  })
+);
---
+++ faust-example-app/wp-templates/page.js
@@ -0,0 +1,4 @@
+
+export default function Component(props) {
+  return null;
+}
